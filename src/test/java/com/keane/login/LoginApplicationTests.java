5761070ae2773ae0361cd6dbd42ad29b65dfd39c
keane <keanetay@gmail.com>
2023-07-27 20:35:30 +0800

    initial commit

---
+++ src/test/java/com/keane/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keane.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

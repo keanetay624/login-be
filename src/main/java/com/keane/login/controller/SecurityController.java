f581962840872a0d8a323431a4eed2505a6f3638
keane <keanetay@gmail.com>
2023-08-02 22:30:23 +0800

    refactor HelloWorldController.java to SecurityController.java

--- src/main/java/com/keane/login/controller/SecurityController.java
+++ src/main/java/com/keane/login/controller/SecurityController.java
@@ -1,35 +1,30 @@
 package com.keane.login.controller;
 
+import com.keane.login.dto.ApiResponse;
 import com.keane.login.dto.AuthRequest;
 import com.keane.login.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequiredArgsConstructor
-public class HelloWorldController {
+public class SecurityController {
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
 
-    @GetMapping("/")
-    public String hello() {
-        return "hello world";
-    }
-
     @PostMapping("/authenticate")
-    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
+    public ApiResponse authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
 
-        if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getUsername());
-        } else {
-            throw new UsernameNotFoundException("Invalid user request!");
-        }
+        return new ApiResponse
+                .ApiResponseBuilder(HttpStatus.OK)
+                .setAccToken(jwtService.generateToken(authRequest.getUsername()))
+                .build();
     }
 }
